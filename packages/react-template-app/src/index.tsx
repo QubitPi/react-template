4f00fe359a351813a19988508c004b985c769389
Jack <jack20220723@gmail.com>
2023-12-14 21:14:47 +0800

    Configure ESLint & Prettier

--- packages/react-template-app/src/index.tsx
+++ packages/react-template-app/src/index.tsx
@@ -17,5 +17,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+/* eslint-disable  @typescript-eslint/non-nullable-type-assertion-style */
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(<App />);
