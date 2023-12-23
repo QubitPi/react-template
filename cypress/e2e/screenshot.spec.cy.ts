7ddf60fe951a4a563d631f174f65985298760b16
Jack <jack20220723@gmail.com>
2023-12-23 18:32:04 +0800

    Add setup

---
+++ cypress/e2e/screenshot.spec.cy.ts
@@ -0,0 +1,21 @@
+// Copyright 2023 Paion Data. All rights reserved.
+
+import "@argos-ci/cypress/support";
+
+describe("Argos tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("test", () => {
+    const baseUrl = "http://localhost:3000";
+    const pages = [{ name: "homepage", path: "/" }];
+
+    for (const { name, path } of pages) {
+      it(`Screenshots for ${name}`, () => {
+        cy.visit(`${baseUrl}${path}`);
+        cy.argosScreenshot(name);
+      });
+    }
+  });
+});
